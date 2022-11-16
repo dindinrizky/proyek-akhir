6034310f13ff535d149f2ab86702dee39e7d796a
dindinrizky <118289021+dindinrizky@users.noreply.github.com>
2022-11-16 08:17:53 +0700

    Create welcome.blade.php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends("layouts.main")
+
+
+@section('first-content')
+    <div class="card">
+        <div class="card-header">
+            Dasboard
+        </div>
+        <div class="card-body">
+            Selamat Dagang
+        </div>
+    </div>
+@endsection
