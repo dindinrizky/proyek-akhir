25d356f81b051a6b04629c0da56a1f34edaaff7f
dindinrizky <118289021+dindinrizky@users.noreply.github.com>
2022-11-16 08:17:46 +0700

    Create index.blade.php

---
+++ resources/views/person/index.blade.php
@@ -0,0 +1,11 @@
+@extends("layouts.main")
+@section('first-content')
+    <div class="card">
+        <div class="card-header">
+            Dasboard
+        </div>
+        <div class="card-body">
+            ini view
+        </div>
+    </div>
+@endsection
